7523f11dcbaf46f1a7ffeb876350e02c26b03d3d
Keyla <mateoluiskeyla@gmail.com>
2024-06-23 13:28:01 -0500

    Puse donde el usuario ingreso su nombre y contraseña

--- src/App.java
+++ src/App.java
@@ -1,19 +1,32 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class App {
  public static void main(String[] args) {
 
-    ArrayList<String[]> user = new ArrayList<>();
+    ArrayList<String[]> userList = new ArrayList<>();
 
     String[] user1 = {"Jhordan","123"};
     String[] user2 = {"Manuel","145"};
     String[] user3 = {"Mirella","12"};
 
-    user.add(user1);
-    user.add(user2);
-    user.add(user3);
+    userList.add(user1);
+    userList.add(user2);
+    userList.add(user3);
 
-    
-  
+    //crear varible y poner valor 
+
+    String password, user;
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Ingrese Usuario");
+    user =sc.next();
+    System.out.println("Ingrese contraseña");
+    password=sc.next();
+
+    System.out.println("La contrasaña es "+password);
+    System.out.println("El usuario es "+user );
+
  }
 }
