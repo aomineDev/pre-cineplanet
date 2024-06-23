7c5ff4ecc88716d25ece3b6e3b221afb591926b5
Jhordan <calixto4211@gmail.com>
2024-06-23 13:01:59 -0500

    create bd

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+
 public class App {
  public static void main(String[] args) {
-  System.out.println("Hello world! :D !");
+
+    ArrayList<String[]> user = new ArrayList<>();
+
+    String[] user1 = {"Jhordan","123"};
+    String[] user2 = {"Manuel","145"};
+    String[] user3 = {"Mirella","12"};
+
+    user.add(user1);
+    user.add(user2);
+    user.add(user3);
+
+    
+  
  }
 }
