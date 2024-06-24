72e2a7fc63efd6fb0aebb358a9cfec3f51ed2a7c
aomine <aomine1745@gmail.com>
2024-06-24 02:54:09 -0500

    añandiendo comentarios y archivos de resumen en excalidraw

--- src/App.java
+++ src/App.java
@@ -3,65 +3,65 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-
+        // BASE DE DATOS
+		// Usuarios
         ArrayList<String[]> userList = new ArrayList<>();
 
+		// usuarios
         String[] user1 = { "Jhordan", "123" };
         String[] user2 = { "Manuel", "145" };
         String[] user3 = { "Mirella", "12" };
         String[] user4 = { "Roxi", "14" };
 
+		// añadiedo usuarios a la base de datos
         userList.add(user1);
         userList.add(user2);
         userList.add(user3);
         userList.add(user4);
 
+		// imprimiendo tamaña de base de datos y todos los nombres de los usuarios
         System.out.println(userList.size());
         for (String[] user : userList) {
-
             System.out.println(user[0]);
-            
         }
 
-        // crear varible y poner valor
-
+		// LOGIN
+        // variables
         String password, username;
-
         Scanner sc = new Scanner(System.in);
+
+		// obteniendo usuario y contraseña por teclado
         System.out.println("Ingrese Usuario");
         username = sc.next();
         System.out.println("Ingrese contraseña");
         password = sc.next();
 
-        // Validación del username
+        // Validación del nombre de usuario
         boolean userExist = false;
         String []user=new String[2];
 
+		// recorrer todos los usuarios
         for (String[] u: userList) {
-
+			// buscar algun usuario de base de datos que sea igual al ingresado por teclado
             if (username.equals(u[0])) {
-
                 userExist = true;
-                user = u; //user almacena el usuario encontrado (solo 1) a los elementos de u de arrays string
+                user = u; //user almacena el usuario encontrado
             }
         }
 
         if (userExist) {
             System.out.println("El usuario existe");
 
-        //Validar contraseña 
-        if ( password.equals(user[1])){
-            System.out.println("BIENVENIDO");
-        }else{
-            System.out.println("La contraseña no valido");
-        }
-
+			//Validar contraseña 
+			if ( password.equals(user[1])) {
+				System.out.println("BIENVENIDO");
+			} else {
+				System.out.println("La contraseña no valido");
+			}
         } else {
             System.out.println("El usuario no existe");
         }
 
-        
-        
-
+		sc.close();
     }
 }
