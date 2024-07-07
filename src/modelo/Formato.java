05834d832be3610df87c52f9fe3d4ee1654152a6
Roxana <roxivhavez@gmail.com>
2024-07-07 12:03:22 -0500

    Agregue clase formato

---
+++ src/modelo/Formato.java
@@ -0,0 +1,22 @@
+package modelo;
+
+import java.util.ArrayList;
+
+public class Formato {
+    private String formato;
+    private ArrayList<Fecha> fechalist;
+
+    public Formato(String formato, ArrayList<Fecha> fechalist){
+        this.formato = formato;
+        this.fechalist = fechalist;
+    }
+
+    public String getFormato() {
+        return formato;
+    }
+    
+    public ArrayList<Fecha> getFechalist() {
+        return fechalist;
+    }
+
+}
