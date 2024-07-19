5e6a0c381672b3a71915fa25a7ddde9438c5d875
Manuel <manuel_tv_123otmail.com>
2024-07-19 16:24:53 -0500

    termine el modelo butaca

--- src/modelo/Butaca.java
+++ src/modelo/Butaca.java
@@ -4,7 +4,29 @@ public class Butaca {
     
     private int butacaId;
     private int numeroSala;
-    private String [][] butacas;
+    private String [][] butacas = {
+        {"V","L","L","L","L","L","V","V","V","V","V","V","L","L","L","L","V","V","V"},
+        {"V","L","L","L","L","L","L","L","L","L","L","L","L","L","L","L","V","V","V"},
+        {"V","L","L","L","L","L","L","L","L","L","L","L","L","L","L","L","V","V","V"},
+        {"V","L","L","L","L","L","L","L","L","L","L","L","L","L","L","L","V","V","V"},
+        {"V","L","L","L","L","L","L","L","L","L","L","L","L","L","L","L","L","L","L"},
+        {"V","L","L","L","L","L","L","L","L","L","L","L","L","L","L","L","L","L","L"},
+        {"V","L","L","L","L","L","L","L","L","L","L","L","L","L","L","L","L","L","L"},
+        {"L","L","L","L","L","L","L","L","L","L","L","L","L","L","L","L","L","L","L"},
+    };
+    public Butaca(int butacaId,int 
+    numeroSala){
+        this.butacaId= butacaId;
+        this.numeroSala= numeroSala;
+    }
+    public int getButacaId() {
+        return butacaId;
+    }
+    public int getNumeroSala() {
+        return numeroSala;
+    }
+    public String[][] getButacas(){
+        return butacas;
+    }
 
-    
 }
