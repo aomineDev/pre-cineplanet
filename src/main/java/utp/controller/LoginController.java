38c1bccf2339cdffb39885314211065517b0afa3
Jhordan <calixto4211@gmail.com>
2024-07-23 10:43:41 -0500

    culmine el controlador

--- src/main/java/utp/controller/LoginController.java
+++ src/main/java/utp/controller/LoginController.java
@@ -1,44 +1,39 @@
 package utp.controller;
 
-// import java.util.ArrayList;
+import java.util.ArrayList;
 
-// import utp.database.CineplanetDB;
-// import utp.model.User;
+import utp.database.CineplanetDB;
+import utp.model.User;
 
 public class LoginController {
 
-    // //llamada (Instancia unica)
-    // CineplanetDB cineplanetDB;
-    // ArrayList<User> userList;
+    //llamada (Instancia unica)
+    CineplanetDB cineplanetDB;
+    ArrayList<User> userList;
 
-    // //constructor vacio
-    // public LoginController () {
+    //constructor vacio
+    public LoginController () {
 
-    //     cineplanetDB = CineplanetDB.getInstance();
-    //     userList = cineplanetDB.getUserList();
+        cineplanetDB = CineplanetDB.getInstance();
+        userList = cineplanetDB.getUserList();
 
-    // }
+    }
 
-    // public User verifyUsername(String username) {
+    //verificacion del usurname
+    public User verifyUsername(String username) {
+        for (User user : userList) {
 
-    //     for (User user : userList) {
+            if (username.equals(user.getUsername())) {
+                return user;
+            }
+            
+        }
+        return null;
+    }
 
-    //         if (username.equals(user.getUsername())) {
-
-    //             return user;
-
-    //         }
-
-    //     }
-
-    //     return null;
-
-    // }
-
-    // public boolean verifyPassword(User user, String password) {
-        
-    //     return password.equals(user.getPassword());
-
-    // }
-    
+    //validacion del password del usuario encontrado
+    public boolean verifyPassword(User user, String password) {
+     
+        return password.equals(user.getPassword());
+    }
 }
