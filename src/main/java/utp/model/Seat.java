5e81ad982793ee66cafb382041850801d94ba3eb
Manuel <manuel_tv_123otmail.com>
2024-07-23 01:43:34 -0500

    model seat realizado

---
+++ src/main/java/utp/model/Seat.java
@@ -0,0 +1,28 @@
+package utp.model;
+
+public class Seat {
+    private int seatId;
+    private int roomNumber;
+    private String[][] seats = new String[][]{{"V", "L", "L", "L", "L", "L", "V", "V", "V", "V", "V", "V", "L", "L", "L", "L", "V", "V", "V"}, {"V", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "V", "V", "V"}, {"V", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "V", "V", "V"}, {"V", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "V", "V", "V"}, {"V", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L"}, {"V", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L"}, {"V", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L"}, {"L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L"}};
+    
+    public Seat(int seatId, int roomNumber) {
+       this.seatId = seatId;
+       this.roomNumber = roomNumber;
+    }
+ 
+    public int getSeatId() {
+       return this.seatId;
+    }
+ 
+    public int getRoomNumber() {
+       return this.roomNumber;
+    }
+ 
+    public String[][] getSeats() {
+       return this.seats;
+    }
+ 
+    public void updateSeatStatus(int row, int column) {
+       this.seats[row][column] = "O";
+    }
+}
