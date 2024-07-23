b3fb18be44053e7c8600ff8c018e92c93b234015
Keyla <mateoluiskeyla@gmail.com>
2024-07-23 17:28:55 -0500

    Vista estatica de butacas

--- src/main/java/utp/App.java
+++ src/main/java/utp/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("loginView"));
+        scene = new Scene(loadFXML("seatView"));
         stage.setTitle("Cineplanet");
         stage.setScene(scene);
         stage.show();
---
+++ src/main/java/utp/view/SeatView.java
@@ -0,0 +1,18 @@
+package utp.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.text.Text;
+
+public class SeatView {
+
+    @FXML
+    private FlowPane fpSeatContainer;
+
+    @FXML
+    private Text tRoomNumber;
+
+    @FXML
+    private Text tSelectedSeats;
+    
+}
