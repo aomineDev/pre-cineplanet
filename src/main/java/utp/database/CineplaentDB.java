ce5f7293c6c55dd954e5d0ffd2557d5872c81302
Roxana <roxivhavez@gmail.com>
2024-07-23 09:44:08 -0500

    agregue el patron singelton en base de datos

--- src/main/java/utp/database/CineplaentDB.java
+++ src/main/java/utp/database/CineplaentDB.java
@@ -1,5 +1,18 @@
 package utp.database;
 
 public class CineplaentDB {
+
+    private static CineplaentDB instance;
+
+  private CineplaentDB () {
+
+  }
+  public static CineplaentDB getInstance() {
+    if (instance == null) {
+        instance = new CineplaentDB();
+    }
+    return instance;
+  }
   
+
 }
